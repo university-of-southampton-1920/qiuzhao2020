58289cde4f2106bc5b6f8920fa0302a2e7912d2c
RUN <50537048+runorz@users.noreply.github.com>
2020-02-24 21:49:06 +0000

    Update runorz_174.py

--- 2020_02_24/runorz_174.py
+++ 2020_02_24/runorz_174.py
@@ -1,10 +1,6 @@
 class Solution:
   def calculateMinimumHP(self, dungeon):
     need = [[None for _ in range(len(dungeon[0]))]for _ in range(len(dungeon))]
-    # for i in range(len(need[len(dungeon)])):
-    #   need[len(dungeon)][i] = 1
-    # for i in range(len(need)):
-    #   need[i][len(dungeon[0])] = 1
     for rindex in range(len(dungeon)-1, -1, -1):
       for cindex in range(len(dungeon[0])-1, -1, -1):
         if rindex == len(dungeon)-1 and cindex == len(dungeon[0]) - 1:
@@ -19,5 +15,4 @@ class Solution:
         else:
           n = min(need[rindex+1][cindex], need[rindex][cindex+1]) - dungeon[rindex][cindex]
           need[rindex][cindex] = 1 if n <= 0 else n
-    print(need)
     return need[0][0]
