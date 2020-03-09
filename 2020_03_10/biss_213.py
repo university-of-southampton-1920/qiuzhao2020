a0b76ca65a665086f1f00cc732f3568ee43fceac
JyiHUO <879837607@qq.com>
2020-03-09 23:38:37 +0000

    biss_213 O(n^2) -> O(n)

--- 2020_03_10/biss_213.py
+++ 2020_03_10/biss_213.py
@@ -5,20 +5,23 @@ class Solution:
             return 0
         if L == 1:
             return nums[0]
-        dp = [0 for i in range(L)]
         # rob the first one
-        dp[0] = nums[0]
-        res = dp[0]
+        pp = nums[0]
+        p = 0
+        res = nums[0]
         for i in range(2, L-1):
-            for j in range( i-1):
-                dp[i] = max(dp[i], dp[j] + nums[i])
-                res = max(res, dp[i])
+            tmp = pp + nums[i]
+            pp = max(p, pp)
+            p = tmp
+            res = max(res, p, pp)
+        
         # do not rob the first one
-        dp = [0 for i in range(L)]
-        dp[1] = nums[1]
-        res = max(res, dp[1])
+        pp = 0
+        p = nums[1]
+        res = max(res, pp, p)
         for i in range(2, L):
-            for j in range(i-1):
-                dp[i] = max(dp[i], dp[j] + nums[i])
-                res = max(res, dp[i])
+            tmp = pp + nums[i]
+            pp = max(p, pp)
+            p = tmp
+            res = max(res, p, pp)
         return res
