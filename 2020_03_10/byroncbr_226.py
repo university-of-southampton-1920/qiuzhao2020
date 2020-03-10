d7599fb8c2c1dcf0653f5244b29a2971fab6a23c
bangrenc <834392125@qq.com>
2020-03-10 23:33:56 +0800

    Add files via upload

---
+++ 2020_03_10/byroncbr_226.py
@@ -0,0 +1,19 @@
+"""
+Name: byroncbr_226.py
+Author: bangrenc
+Time: 10/3/2020 11:16 PM
+"""
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
+
+
+
