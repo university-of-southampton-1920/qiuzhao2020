f6b627d2f86ada131cf40a996c9a08bac8d6ad7c
bangrenc <834392125@qq.com>
2020-03-15 19:44:09 +0800

    Create byroncbr_21.py

---
+++ 2020_03_15/byroncbr_21.py
@@ -0,0 +1,28 @@
+"""
+Name: byroncbr_21.py
+Author: bangrenc
+Time: 15/3/2020 7:29 PM
+"""
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = cur = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 or l2
+        return dummy.next
+
+
+
+
