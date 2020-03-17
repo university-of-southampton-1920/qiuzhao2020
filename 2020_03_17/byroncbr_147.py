d02122b5b7224fbdd825c700e7cad1c224341344
bangrenc <834392125@qq.com>
2020-03-17 16:31:34 +0800

    Add files via upload

---
+++ 2020_03_17/byroncbr_147.py
@@ -0,0 +1,31 @@
+"""
+Name: byroncbr_147.py
+Author: bangrenc
+Time: 17/3/2020 4:30 PM
+"""
+
+#need to re-run
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        p = dummy = ListNode(0)
+        cur = dummy.next = head
+        while cur and cur.next:
+            val = cur.next.val
+            if cur.val < val:
+                cur = cur.next
+                continue
+            if p.next.val > val:
+                p = dummy
+            while p.next.val < val:
+                p = p.next
+            new = cur.next
+            cur.next = new.next
+            new.next = p.next
+            p.next = new
+        return dummy.next
