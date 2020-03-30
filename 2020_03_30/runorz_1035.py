6e002a55924832aa775972e6402f64bd4c1d2e05
RUN <50537048+runorz@users.noreply.github.com>
2020-03-30 11:26:41 +0100

    Create runorz_1035.py

---
+++ 2020_03_30/runorz_1035.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxUncrossedLines(self, A: List[int], B: List[int]) -> int:
+        dp = [[0 for _ in range(len(B)+1)]for _ in range(len(A)+1)]
+        for i in range(len(A)):
+            for j in range(len(B)):
+                if A[i] == B[j]:
+                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j]+1)
+                else:
+                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+        return dp[len(A)][len(B)]
