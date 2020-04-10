7423f6200ffa8e7b67a412892222aa87f21e9ddb
SauryStand <voyager2511@outlook.com>
2020-04-10 21:48:10 +0100

    update:300

---
+++ 2020_04_09/saurystand_300.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            while i != j:
+                m = int((i + j) / 2)
+                if tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = x
+            size = max(i + 1, size)
+        return size
+
+    def solution2(nums):
+        if not nums: return 0
+        dp = [0] * (len(nums) + 1)
+        n = len(nums)
+        for i in range(n):
+            # 每层开始搜索
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
