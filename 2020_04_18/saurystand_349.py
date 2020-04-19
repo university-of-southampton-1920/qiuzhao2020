bf122e854ecc4a6e177c79e60fd400e0370d3d34
Saurystand <voyager2511@outlook.com>
2020-04-20 00:06:52 +0100

    349, find out duplicated nums

---
+++ 2020_04_18/saurystand_349.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
+        i, j = 0, 0
+        n1, n2 = len(nums1), len(nums2)
+        res = set()
+        while i < n1 and j < n2:
+            if nums1[i] == nums2[j]:
+                res.add(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+
+        return res  # nlogn no good
