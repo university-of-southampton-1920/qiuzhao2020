cc3cf64debc7049b83c83c2558494f3d61628986
Saurystand <voyager2511@outlook.com>
2020-05-23 23:45:44 +0100

    tree

---
+++ 2020_05_22/saurystand_114.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        def helper(root):
+            if root == None: return
+            helper(root.left)
+            helper(root.right)
+            if root.left != None:
+                pre = root.left
+                while pre.right:
+                    pre = pre.right
+                pre.right = root.right
+                root.right = root.left
+                root.left = None
+            root = root.right
+        helper(root)
