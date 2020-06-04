60b49ab73b74bdf8f5fce5f1f1f9a8f22dea77c4
JyiHUO <879837607@qq.com>
2020-06-04 23:29:51 +0800

    biss_238.py

---
+++ 2020_06_04/biss_238.py
@@ -0,0 +1,11 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [0 for i in range(len(nums))]
+        res[0] = 1
+        for i in range(1, len(nums)):
+            res[i] = res[i-1] * nums[i-1]
+        right = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] = right * res[i]
+            right = right * nums[i]
+        return res
